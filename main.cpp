3ebd288623aba02e79b4e78c51c44a64891ed41d
Abe Winter <abe-winter@users.noreply.github.com>
2024-09-03 15:44:35 -0400

    example working offline (with dns trick)

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 
 using namespace viam::sdk;
 
-std::string getenv_str(const char* name) {
+std::string getenv_str(const char *name)
+{
     return std::getenv(name);
 }
 
@@ -14,12 +15,17 @@ int main()
     std::string host(getenv_str("VIAM_HOST"));
     Credentials credentials("api-key", getenv_str("VIAM_API_KEY"));
     DialOptions dial_opts;
-    dial_opts.set_entity(getenv_str("VIAM_ENTITY_ID"));
-    dial_opts.set_credentials(credentials);
-    boost::optional<DialOptions> opts(dial_opts);
-    Options options(0, opts);
+    if (credentials.payload().empty())
+    {
+        dial_opts.set_allow_insecure_downgrade(true);
+    }
+    else
+    {
+        dial_opts.set_entity(getenv_str("VIAM_ENTITY_ID"));
+        dial_opts.set_credentials(credentials);
+    }
 
-    auto machine = RobotClient::at_address(host, options);
+    auto machine = RobotClient::at_address(host, Options(0, dial_opts));
 
     std::cout << "Resources:\n";
     for (const Name &resource : machine->resource_names())
